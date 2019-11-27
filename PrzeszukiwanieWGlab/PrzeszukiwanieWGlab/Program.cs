b09722984d52737d18570c687d55df095c9a6d7d
PatrykRed <48053600+PatrykRed@users.noreply.github.com>
2019-11-27 19:37:05 +0100

    Add files via upload

---
+++ PrzeszukiwanieWGlab/PrzeszukiwanieWGlab/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzeszukiwanieWGlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Node
+    {
+        public int wartosc;
+
+        public Node(int wartosc)
+        {
+            this.wartosc = wartosc;
+        }
+
+        public override string ToString()
+        {
+            return this.wartosc.ToString();
+        }
+        
+    }
+
+    class Krawędź
+    {
+        public Node start;
+        public Node koniec;
+        public int waga;
+        public Krawędź(Node s, Node k, int w=1)
+        {
+            this.start = s;
+            this.koniec = k;
+            this.waga = w;
+        }
+        public override string ToString()
+        {
+            return $"{this.start}-{this.koniec}({this.waga})";
+        }
+
+        public Node ZnajdźDrugi(Node n)
+        {
+            return this.start == n ? this.koniec : this.start;
+
+            if(this.start==n)
+            {
+                return this.koniec;
+            
+            }
+            else
+                return this.start;
+        }
+    }
+
+}
